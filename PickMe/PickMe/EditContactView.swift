4f399a0e599356b4cb33caaef4e16521c76f084f
LuthforKhan <lrkhan@umich.edu>
2022-03-28 14:35:50 -0400

    test

---
+++ PickMe/PickMe/EditContactView.swift
@@ -0,0 +1,20 @@
+//
+//  EditContactView.swift
+//  PickMe
+//
+//  Created by Luthfor Khan on 3/28/22.
+//
+
+import SwiftUI
+
+struct EditContactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditContactView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditContactView()
+    }
+}
